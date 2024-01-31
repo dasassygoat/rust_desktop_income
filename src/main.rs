79d3f7902199a1fd252b5ea20ba49b071faa2521
Bryanp <dasassygoat@coderbox.co>
2024-01-30 21:43:32 -0600

    wasm

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-fn main() {
+#[cfg_attr(target_arch = "wasm32",
+wasm_bindgen::prelude::wasm_bindgen(start))]
+pub fn main() {
     // Initializes the main window, fetches tile data from it, duplicates and
     // shuffles the tiles randomly, then assigns the shuffled tiles back to the
     // main window model before running the main window.
